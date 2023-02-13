b3c0772d894896d62d4e303ab749e215e282ca75
JavierGoodall <javiergoodall@outlook.com>
2023-02-13 16:05:13 +0200

    First Commit

---
+++ app/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    HOST: "bykfaikcypr9qzv50p1u-mysql.services.clever-cloud.com",
+    USER: "ue2azizu96a80jfg",
+    PASSWORD: "kk4Q0cbAP9yq9vaMkKHc",
+    DB: "bykfaikcypr9qzv50p1u",
+    dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  };
